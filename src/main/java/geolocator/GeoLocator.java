2ac25987c4b7476eeaf5d32c4d20363e2d776937
Kausar Ali <naqviali670@gmail.com>
2020-04-04 14:35:09 +0200

    Optional assignment

--- src/main/java/geolocator/GeoLocator.java
+++ src/main/java/geolocator/GeoLocator.java
@@ -1,3 +1,4 @@
+
 package geolocator;
 
 import java.net.URL;
@@ -9,37 +10,56 @@ import com.google.gson.Gson;
 import com.google.common.net.UrlEscapers;
 
 import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class GeoLocator {
 
     public static final String GEOLOCATOR_SERVICE_URI = "http://ip-api.com/json/";
 
+    private static Logger logCreator = LoggerFactory.getLogger(GeoLocator.class);
     private static Gson GSON = new Gson();
 
     public GeoLocator() {}
 
-    public GeoLocation getGeoLocation() throws IOException {
-        return getGeoLocation(null);
+    public GeoLocation getGeoLocation() {
+        try {
+            return getGeoLocation(null);
+        } catch (IOException e) {
+            logCreator.error("IOException", new RuntimeException(e.getMessage()));
+        }
+        return null;
     }
 
     public GeoLocation getGeoLocation(String ipAddrOrHost) throws IOException {
-        URL url;
+
+        URL url = null;
+        String s = new String();
         if (ipAddrOrHost != null) {
+            logCreator.info("Parsing url");
             ipAddrOrHost = UrlEscapers.urlPathSegmentEscaper().escape(ipAddrOrHost);
-            url = new URL(GEOLOCATOR_SERVICE_URI + ipAddrOrHost);
+                logCreator.debug("Debug Message");
+                url = new URL(GEOLOCATOR_SERVICE_URI + ipAddrOrHost);
+
         } else {
-            url = new URL(GEOLOCATOR_SERVICE_URI);
+
+                url = new URL(GEOLOCATOR_SERVICE_URI);
+
         }
-        String s = IOUtils.toString(url, "UTF-8");
+
+            s = IOUtils.toString(url, "UTF-8");
+        logCreator.info("returning information");
         return GSON.fromJson(s, GeoLocation.class);
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
         try {
-            String arg = args.length > 0 ? args[0] : null;
+        String arg = args.length > 0 ? args[0] : null;
+        logCreator.info("Geolocation info...");
+
             System.out.println(new GeoLocator().getGeoLocation(arg));
         } catch (IOException e) {
-            System.err.println(e.getMessage());
+            logCreator.error("IOException", new RuntimeException(e.getMessage()));
         }
     }
 
